5ee4a6cf1268fe107cbab11df5afb04267ea77cd
anthony <anthonyjimerson42@gmail.com>
2023-07-12 14:19:55 -0500

    updated, samerror

--- app.js
+++ app.js
@@ -4,30 +4,33 @@ const movieResultsEl = document.querySelector(" .movie__results")
 const searchTitleEl = document.querySelector(" .title")
 const loaderSpinEl = document.querySelector(" .loader")
 const sortEl = document.querySelector("#sort")
+let keyword = ""
 
 async function renderMovies (filter) {
 
+    console.log(filter)
 
     const URL = `https://www.omdbapi.com/?s=${keyword}&page=1&apikey=867f9b9b`
     const res = await fetch(`${URL}`)
     const data = await res.json()
     const movieListEl = document.querySelector(".result__container")
    
-    movieListEl.innerHTML = data.Search.map((movie) => moviesHTML(movie)).join("")
-
+    
     if(sort === "NEW_TO_OLD") {
-        data.sort ((a, b) => b.Year - a.Year)
+        data.Search.sort ((a, b) => b.Year - a.Year)
     }
     else if (sort === "OLD_TO_NEW") {
-        data.sort ((a, b) => a.Year - b.Year)
+        data.Search.sort ((a, b) => a.Year - b.Year)
     }
-
-           
+    
+    
+    movieListEl.innerHTML = data.Search.map((movie) => moviesHTML(movie)).join("")
 
 }
 
-function sortMovieYear (event) {
-    renderMovies(event.target.value)
+function sortMovieYear () {
+    const sortValue = sortEl.value;
+    renderMovies(sortValue)
 }
 
 
